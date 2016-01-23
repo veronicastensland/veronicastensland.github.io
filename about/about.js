024331b5965547acbce0dd97f448aa7e499126fb
Amanda <amanda.stensland@publit.se>
2016-01-23 12:22:29 +0100

    Adds more pages

---
+++ about/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name veronicastenslandApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the veronicastenslandApp
+ */
+angular.module('veronicastenslandApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ main/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name veronicastenslandApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the veronicastenslandApp
+ */
+angular.module('veronicastenslandApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
